e17cd34898f3c3a9d504d2222d07823f5edc1563
Bogdan Yakupov <yourinfinityuser@gmail.com>
2021-01-22 07:17:35 +0600

    collecting shell trouble fixed

--- collect.sh
+++ collect.sh
@@ -2,7 +2,7 @@ logs="./dumps/log"
 var=0
 
 while true; do
-	./debug nv ./dumps/imprint$var 2>$logs
+	./debug nv ./dumps/imprint$var 2>$logs &
 	sleep 600
 	ps -ef | grep ./debug | grep -v grep | awk '{ print $2 }' | xargs kill -2
 	((var=$var+1))
